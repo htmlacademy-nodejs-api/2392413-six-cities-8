bba0e30df6934e133d3e363012d1480e9e4f1194
skudinva <skudinva@gmail.com>
2024-09-19 11:27:12 +0700

    ++createdAt

--- src/shared/modules/user/user-model.ts
+++ src/shared/modules/user/user-model.ts
@@ -1,14 +1,20 @@
 import { Document, model, Schema } from 'mongoose';
 import { User } from '../../types/index.js';
 
-export interface UserDocument extends User, Document {}
+export interface UserDocument extends User, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
 
-const userSchema = new Schema({
-  name: String,
-  email: String,
-  avatarUrl: String,
-  password: String,
-  isPro: Boolean,
-});
+const userSchema = new Schema(
+  {
+    name: String,
+    email: String,
+    avatarUrl: String,
+    password: String,
+    isPro: Boolean,
+  },
+  { timestamps: true }
+);
 
 export const UserModel = model<UserDocument>('User', userSchema);
