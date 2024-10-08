222f50cd46d67443861be91a96f357148522cb12
skudinva <skudinva@gmail.com>
2024-10-08 20:48:52 +0700

    ++

--- src/shared/modules/review/rdo/create-review-rdo.ts
+++ src/shared/modules/review/rdo/create-review-rdo.ts
@@ -1,11 +1,19 @@
 import { ReviewEntity } from '#modules/review/review-entity.js';
-import { UserEntity } from '#modules/user/user-entity.js';
-import { Ref } from '@typegoose/typegoose';
+import { Expose, Type } from 'class-transformer';
+import { UserRdo } from '../../user/rdo/user-rdo.js';
 
-type reviewExcludeType = 'id' | '_id' | 'createdAt' | 'updatedAt';
+type reviewExcludeType = 'id' | '_id' | 'userId' | 'createdAt' | 'updatedAt';
 export class ReviewRdo implements Omit<ReviewEntity, reviewExcludeType> {
+  @Expose()
   public date: Date;
-  public userId: Ref<UserEntity>;
+
+  @Expose({ name: 'userId' })
+  @Type(() => UserRdo)
+  public user: UserRdo;
+
+  @Expose()
   public comment: string;
+
+  @Expose()
   public rating: number;
 }
